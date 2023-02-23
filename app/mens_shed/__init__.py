b7767670fbe222f7ba8ee16654e4da881318b622
RoryM18 <rorymac126@gmail.com>
2023-02-23 21:59:54 +0000

    Created an empty database. Still to be populated

--- app/mens_shed/__init__.py
+++ app/mens_shed/__init__.py
@@ -1,9 +1,14 @@
 #This is the file which tells python that this is a package and also initalises and ties together everything we need for the app
-
+import sqlite3
 from flask import Flask
 
 
 app = Flask(__name__)
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 #The Routes import must be kept after the app declaration otherwise you will get a circular import error
 from mens_shed import route
