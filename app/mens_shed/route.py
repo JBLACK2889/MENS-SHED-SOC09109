1c8069b74560fd83e97024f597dd62f636f4d94e
RoryM18 <rorymac126@gmail.com>
2023-02-25 18:28:16 +0000

    Changed around front end files and code so that webapp can display work done

--- app/mens_shed/route.py
+++ app/mens_shed/route.py
@@ -4,16 +4,39 @@ from flask import render_template
 from mens_shed import app
 import sqlite3
 
+#Set up database connection
 def get_db_connection():
     conn = sqlite3.connect('database.sqlite')
     conn.row_factory = sqlite3.Row
     return conn
 
+
 #This is the home page route
 @app.route("/")
 @app.route("/home")
 def home():
-    conn = get_db_connection()
-    users = conn.execute('SELECT * FROM Users').fetchall()
-    conn.close()
-    return render_template('home.html', users=users)
+    return render_template('home.html')
+
+
+#This is the about page route
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+#This is the booking page route
+@app.route("/booking")
+def booking():
+    return render_template('booking.html', title='Booking')
+
+
+#This is the resource page route
+@app.route("/resource")
+def resource():
+    return render_template('resource.html', title='Resources')
+
+
+#This is the support page route
+@app.route("/support")
+def support():
+    return render_template('support.html', title='Support')
