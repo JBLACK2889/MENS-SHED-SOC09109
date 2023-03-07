55f9419329613be967730a7ede435c2ad8356b21
RoryM18 <40492068@live.napier.ac.uk>
2023-03-07 15:38:58 +0000

    changed code

--- app/mens_shed/route.py
+++ app/mens_shed/route.py
@@ -6,7 +6,7 @@ import sqlite3
 
 #Set up database connection
 def get_db_connection():
-    conn = sqlite3.connect('database.sqlite')
+    conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
