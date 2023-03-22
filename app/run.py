85e36851a0affeb30ac3f42587e7c2576e4b5e51
DanBeardm <40435226@live.napier.ac.uk>
2023-03-22 15:48:10 +0000

    Added login

--- app/run.py
+++ app/run.py
@@ -4,3 +4,14 @@ from mens_shed import app
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
+
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['uname'] != 'test' or request.form['pword'] != 'test':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
