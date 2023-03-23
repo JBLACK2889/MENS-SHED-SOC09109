cff18cf97b8bcb1fa871dcd13f8720a905f280e8
RoryM18 <40492068@live.napier.ac.uk>
2023-03-23 23:19:04 +0000

    Added Register and Login features and made majour changed to the home, login, navbar and signup html pages. Database also has the functionality to encrypt passwords using bcrypt

--- app/mens_shed/__init__.py
+++ app/mens_shed/__init__.py
@@ -2,10 +2,19 @@
 import sqlite3
 from flask import Flask
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
+app.config['SECRET_KEY'] = 'SECRET_KEY'
+app.config['DATABASE'] = 'database.db'
+
+# Initialize the LoginManager object
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
 
 #The Routes import must be kept after the app declaration otherwise you will get a circular import error
 from mens_shed import route
