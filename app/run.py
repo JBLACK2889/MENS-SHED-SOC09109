717a3c888bce6d57b356f38231c6105cb0152c57
Napier40 <113589821+Napier40@users.noreply.github.com>
2023-11-09 20:07:05 +0000

    commit

--- app/run.py
+++ app/run.py
@@ -1,7 +1,5 @@
-#This is the file that launches the web application 
-
+"""Module providing a function printing python version."""
 from mens_shed import app
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
-
